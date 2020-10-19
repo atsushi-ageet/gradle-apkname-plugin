839f1e6fe4fe3adbca85e6f95ce2fe3aa8d4e881
atsushi <atsushi@ageet.com>
2020-10-20 00:46:43 +0900

    Fix comments for ApkNameExtension

--- buildSrc/src/main/kotlin/com/ageet/gradle/plugin/apkname/ApkNameExtension.kt
+++ buildSrc/src/main/kotlin/com/ageet/gradle/plugin/apkname/ApkNameExtension.kt
@@ -2,13 +2,6 @@ package com.ageet.gradle.plugin.apkname
 
 /**
  * @property format format of apk name.
- * @property projectName gradle project name.
- * @property applicationId android applicationId.
- * @property versionCode android versionCode.
- * @property versionName android versionName.
- * @property variantName android variantName.
- * @property buildType android buildType.
- * @property gitShortHash git short commit hash.
  */
 open class ApkNameExtension {
     var format: ApkNameFormatter.()->CharSequence = { "${projectName}_${versionName}_${gitShortHash}" }
